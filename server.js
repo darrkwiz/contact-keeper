c2346955be09bd4497e3364e3770b0b95b16b302
darrkwiz <darrkwiz@gmail.com>
2020-07-25 19:59:15 +0400

    connected mongo database

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect DB
+connectDB();
+
 app.get('/', (req,res) => res.json({msg: 'Welcome to the Contact Keeper Api'}));
 
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
