1554f2968bc2bfa85cf6959288cc1fbd0f6e17f8
darrkwiz <darrkwiz@gmail.com>
2020-07-27 10:56:09 +0400

    feature to add contact and retreive contacts

--- models/Contact.js
+++ models/Contact.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ContactSchema = mongoose.Schema({
-    users: {
+    user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users'
     },
