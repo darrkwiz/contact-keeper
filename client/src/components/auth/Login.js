76cd1e7b676e971698283933b3a9bbc8d87ae1c7
darrkwiz <darrkwiz@gmail.com>
2020-08-12 17:23:57 +0400

    added an alerts context api

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+
+    const [user, setUser] = useState({
+        email:'',
+        password: ''
+    })
+
+    const { email, password } = user;
+
+    const onChange = e => setUser({[e.target.name]: e.target.value});
+
+    return (
+        <div className="form-container">
+            <h1>
+                Account <span className="text-primary">Login</span>
+            </h1>
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" value={email} onChange={onChange} />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" value={password} onChange={onChange} />
+            </div>
+            <input type="submit" value="Login" className="btn btn-primary btn-block" />
+        </div>
+    )
+
+}
+
+export default Login;
